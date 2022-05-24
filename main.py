2713e1b16930fcba1fcaa8dd60add89f6399100d
Anatom <49984698+anatom3000@users.noreply.github.com>
2022-05-24 11:35:20 +0200

    added more oscillators

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ NB_HARMONIQUE = 4
 
 bpm = 240
 song = "c3 e3 f3 g3 a3 b3".split() * 10
-env = synth.Enveloppe(attack=0.05, decay=0.1, sustain=0.2, release=0.2)
+env = synth.Enveloppe(attack=0.05, decay=0.1, sustain=0.2, release=0.1)
+env = synth.Enveloppe(attack=0.0, decay=0.0, sustain=1.0, release=0.0)
 
 sound = np.array([])
 
 for i, note in enumerate(song):
-    base = synth.Note.from_string(note, env, 60 / bpm - env.release, oscillators.square)
+    base = synth.Note.from_string(note, env, 60 / bpm - env.release, oscillators.sawtooth)
     t = np.arange(0, base.total_lenght, 1 / SAMPLE_RATE)
 
     sound = np.append(sound, base(t))
--- oscillators.py
+++ oscillators.py
@@ -1,11 +1,24 @@
 import numpy as np
 
 
-def sine(t, freq):
-    return np.sin(t * 2 * np.pi * freq)
+def sine(t, frequency):
+    return np.sin(t * 2 * np.pi * frequency)
 
 
-def square(t, freq):
-    return 1 if t * freq % 1 < (1 / 2) else -1
+def square(t, frequency):
+    return (-1) ** int(t * frequency)
 
-square = np.vectorize(square)
+
+def triangle(t, frequency):
+    return 2 * \
+           np.absolute(
+               t * frequency
+               - np.floor(
+                   t * frequency
+                   + 0.5
+               )
+           )
+
+
+def sawtooth(t, frequency):
+    return (frequency * t) % 1
